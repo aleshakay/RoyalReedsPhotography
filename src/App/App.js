3a2d558274cafbd2336e0ebca0f23609d7ee2db3
alesha reed <aleshak83@gmail.com>
2021-01-23 22:51:49 -0600

    update files

--- src/App/App.js
+++ src/App/App.js
@@ -3,17 +3,20 @@ import {
 BrowserRouter as Router,
 Route
 } from 'react-router-dom';
+import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import './App.scss';
 import HomePage from '../pages/HomePage';
 import AboutPage from '../pages/AboutPage';
 import ArticlePage from '../pages/ArticlePage';
 import ArticlesList from '../pages/ArticlesList';
 import NavBar from '../shared/NavBar';
+import './App.scss';
 
+const initialFormState = { name: '', description: '' }
 
 
 function App() {
+
   return (
     <Router>
     <NavBar />
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ArticlesList from './ArticlesList';
 
 const ArticlePage = () =>(
   <>
     <h1>Articles</h1>
+    <ArticlesList  />
   </>
 );
 
